2b6ead290a4a2170bd6dc9a6a105e6a0b7f5779b
alesandi22 <alesf22@gmail.com>
2022-04-28 10:02:36 -0600

    fixed deploy errors

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,31 +1,13 @@
 import Layout from 'components/layout';
 import Providers from 'components/layout/providers';
-import { getCookie } from 'cookies-next';
 import 'styles/globals.scss';
-import { verify } from 'jsonwebtoken';
 
-export default function MyApp({
-  Component, pageProps, user,
-}) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <Providers>
-      <Layout user={user}>
+      <Layout>
         <Component {...pageProps} />
       </Layout>
     </Providers>
   );
 }
-
-MyApp.get = ({ ctx }) => {
-  let user = null;
-  try {
-    const { req, res } = ctx;
-    const token = getCookie('manager-app-projects-user-token', { req, res });
-    if (!token) return {};
-    user = verify(token, process.env.ACCESS_TOKEN_SECRET);
-  } catch (error) {
-    throw new Error(error.message);
-  }
-
-  return { user };
-};
