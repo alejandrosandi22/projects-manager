ed058d95965ee4ae27e40d4a6ac9cf9cb85045c7
alesandi22 <alesf22@gmail.com>
2022-04-28 15:57:28 -0600

    remove console.log

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -15,8 +15,6 @@ export default function Nav() {
   const [toggle, setToggle] = useState(false);
   const router = useRouter();
 
-  console.log(user)
-
   const hanldeToggle = () => {
     setToggle(!toggle);
   }
